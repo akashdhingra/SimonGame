9046b31b26c9541298a6906dc60abbcaa76abc25
Akash Dhingra <akashdhingra1996@gmail.com>
2022-10-29 12:25:59 -0400

    add function to check if answer is right or wrong

--- game.js
+++ game.js
@@ -3,15 +3,33 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 
 var userClickedPattern = [];
+var started = false;
+var level = 0;
 
-// $(".btn").click(nextSequence);
+$(document).keypress(function(){
+  if(!started){
+    $("#level-title").text("Level "+level);
+    nextSequence();
+    started = true;
+  }
+
+});
+
 $(".btn").click(function(){
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
+
   playSound(userChosenColour);
+  animatePress(userChosenColour);
+
+  checkAnswers(userClickedPattern.length - 1);
 });
 
 function nextSequence(){
+  level++;
+
+  $("#level-title").text("Level "+level);
+
   var randomNumber = Math.floor(Math.random()*4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
@@ -20,12 +38,39 @@ function nextSequence(){
   // playSound(randomChosenColour);
 }
 
-
 function playSound(name){
-  var test1 = new Audio("sounds/" + name +".mp3");
-  var playPromise = test1.play();
+  var audio = new Audio("sounds/" + name +".mp3");
+  var playPromise = audio.play();
 }
 
+function animatePress(currentColour){
+  $("#"+currentColour).addClass("pressed");
+  setTimeout(function(){
+    $("#"+currentColour).removeClass("pressed");},100)
+};
+
+function wrongButton(){
+  $("body").addClass("game-over");
+  setTimeout(function(){
+    $("body").removeClass("game-over");},200)
+};
 
 
-nextSequence();
+function checkAnswers(currentLevel){
+
+  if(userClickedPattern[currentLevel] == gamePattern[currentLevel]){
+      console.log("success");
+      if (userClickedPattern.length == gamePattern.length){
+        setTimeout(function(){
+          nextSequence();
+        }, 1000);
+
+      }
+  }
+  else
+    console.log("wrong");
+    var audio = new Audio("sounds/wrong.mp3");
+    var playPromise = audio.play();
+    wrongButton();
+
+}
