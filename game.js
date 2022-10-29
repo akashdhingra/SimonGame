e6380e812966556df0b0dab72f3a0c93b6b58341
Akash Dhingra <akashdhingra1996@gmail.com>
2022-10-29 10:59:43 -0400

    added sound to the clicks

--- game.js
+++ game.js
@@ -1,14 +1,31 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+
 var gamePattern = [];
+
+var userClickedPattern = [];
+
+// $(".btn").click(nextSequence);
+$(".btn").click(function(){
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+});
 
 function nextSequence(){
   var randomNumber = Math.floor(Math.random()*4);
-  return randomNumber;
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+  // playSound(randomChosenColour);
 }
 
-const buttonColours = ["red", "blue", "green", "yellow"];
 
-var randomChosenColour = buttonColours[nextSequence()];
+function playSound(name){
+  var test1 = new Audio("sounds/" + name +".mp3");
+  var playPromise = test1.play();
+}
 
-gamePattern.push(randomChosenColour);
 
-$("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+nextSequence();
